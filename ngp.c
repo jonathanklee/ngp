jonathanklee/ngp

commit baf210a24bd0434b569a98aae3f3c3bb3a59cbeb

If line is a file, skip it
Fixes #18

ngp.c
@@ -322,13 +322,17 @@ static void page_down(int *index, int *cursor)
 
 static void cursor_up(int *index, int *cursor)
 {
-	if (*cursor == 0) {
-		page_up(index, cursor);
-		return;
-	}
-
 	if (*cursor > 0) {
 		*cursor = *cursor - 1;
+	}
+
+	/* If line is a file, skip it*/
+	if (!strcmp(data.entry[*cursor + *index].line, ""))
+		*cursor = *cursor - 1;
+
+	if (*cursor <= 0) {
+		page_up(index, cursor);
+		return;
 	}
 
 	display_entries(index, cursor);
@@ -344,6 +348,10 @@ static void cursor_down(int *index, int *cursor)
 	if (*cursor + *index < data.nbentry - 1) {
 		*cursor = *cursor + 1;
 	}
+	
+	/* If line is a file, skip it*/
+	if (!strcmp(data.entry[*cursor + *index].line, ""))
+		*cursor = *cursor + 1;
 
 	display_entries(index, cursor);
 }
@@ -492,6 +500,7 @@ void main(int argc, char *argv[])
 			}
 		}
 		usleep(10000);
+		
 		refresh();
 
 		if (data.status == 0 && data.nbentry == 0) {

commit 197772c966e089d8460f193cdb086aa5a6acc19c

Fix bug introduced by commit baf210a24bd0434b569a98aae3f3c3bb3a59cbeb
Used to be that going up when on top of a page skipped the first entry.

ngp.c
@@ -322,6 +322,11 @@ static void page_down(int *index, int *cursor)
 
 static void cursor_up(int *index, int *cursor)
 {
+	if (*cursor == 0) {
+		page_up(index, cursor);
+		return;
+	}
+
 	if (*cursor > 0) {
 		*cursor = *cursor - 1;
 	}
@@ -330,7 +335,7 @@ static void cursor_up(int *index, int *cursor)
 	if (!strcmp(data.entry[*cursor + *index].line, ""))
 		*cursor = *cursor - 1;
 
-	if (*cursor <= 0) {
+	if (*cursor < 0) {
 		page_up(index, cursor);
 		return;
 	}