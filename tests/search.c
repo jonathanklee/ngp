7bebb7c75bf20b5f533e494b9c351082e8ec4781
Jonathan Klee <klee.jonathan@gmail.com>
2016-08-30 20:19:14 +0200

    add two entries unit test

--- tests/search.c
+++ tests/search.c
@@ -54,11 +54,24 @@ static char * test_one_entry_incase() {
     return 0;
 }
 
+static char * test_two_entries() {
+
+    struct search_t *search;
+    search = create_search();
+    char text[] = "this is a the first line\nthis is the second line\n";
+    const char *pattern = "line";
+    parse_text(search, "fake_file", strlen(text), text, pattern);
+    mu_assert("error in number of entry", search->nbentry == 3);
+    free_search(search);
+    return 0;
+}
+
 static char * all_tests() {
     mu_run_test(test_no_entry);
     mu_run_test(test_one_entry_on_the_first_line);
     mu_run_test(test_one_entry_on_the_second_line);
     mu_run_test(test_one_entry_incase);
+    mu_run_test(test_two_entries);
     return 0;
 }
 
