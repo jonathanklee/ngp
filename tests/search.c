54e8e703b4f8e2e000d87f9f5588a6985309b2a4
Jonathan Klee <klee.jonathan@gmail.com>
2016-08-31 20:54:09 +0200

    some moar unit tests

--- tests/search.c
+++ tests/search.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "minunit.h"
 #include "search.h"
+#include "list.h"
 
 int tests_run = 0;
 
@@ -92,6 +93,66 @@ static char * test_wrong_regexp() {
     return 0;
 }
 
+static char * test_is_specific_file_ok() {
+
+    struct search_t *search;
+    search = create_search();
+    add_element(&search->specific_file, "Makefile");
+    mu_assert("test_is_specific_file_ok failed", is_specific_file(search, "Makefile") == 0);
+    free_search(search);
+    return 0;
+}
+
+static char * test_is_specific_file_ko() {
+
+    struct search_t *search;
+    search = create_search();
+    add_element(&search->specific_file, "Makefile");
+    mu_assert("test_is_specific_file_ko failed", is_specific_file(search, "makefile") == 0);
+    free_search(search);
+    return 0;
+}
+
+static char * test_is_ignored_file_ok() {
+
+    struct search_t *search;
+    search = create_search();
+    add_element(&search->ignore, "rules");
+    mu_assert("test_is_ignored_file_ok failed", is_ignored_file(search, "rules") == 0);
+    free_search(search);
+    return 0;
+}
+
+static char * test_is_ignored_file_ko() {
+
+    struct search_t *search;
+    search = create_search();
+    add_element(&search->ignore, "rules");
+    mu_assert("test_is_ignored_file_ko failed", is_ignored_file(search, "Rules") == 0);
+    free_search(search);
+    return 0;
+}
+
+static char * test_is_extension_good_ok() {
+
+    struct search_t *search;
+    search = create_search();
+    add_element(&search->extension, ".cpp");
+    mu_assert("test_is_extension_good_ok failed", is_ignored_file(search, "file.cpp") == 0);
+    free_search(search);
+    return 0;
+}
+
+static char * test_is_extension_good_ko() {
+
+    struct search_t *search;
+    search = create_search();
+    add_element(&search->extension, ".cpp");
+    mu_assert("test_is_extension_good_ko failed", is_ignored_file(search, "file.c") == 0);
+    free_search(search);
+    return 0;
+}
+
 static char * all_tests() {
     mu_run_test(test_no_entry);
     mu_run_test(test_one_entry_on_the_first_line);
@@ -100,6 +161,12 @@ static char * all_tests() {
     mu_run_test(test_two_entries);
     mu_run_test(test_regexp_start_of_line);
     mu_run_test(test_wrong_regexp);
+    mu_run_test(test_is_specific_file_ok);
+    mu_run_test(test_is_specific_file_ko);
+    mu_run_test(test_is_ignored_file_ok);
+    mu_run_test(test_is_ignored_file_ko);
+    mu_run_test(test_is_extension_good_ok);
+    mu_run_test(test_is_extension_good_ko);
     return 0;
 }
 
