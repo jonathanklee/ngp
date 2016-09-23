efd89b5d911a4b95f64964aef74031fffd361559
Jonathan Klee <klee.jonathan@gmail.com>
2016-09-23 22:40:18 +0200

    implement test_cursor_up()

--- tests/main.c
+++ tests/main.c
@@ -265,6 +265,28 @@ static char * test_cursor_down_end_of_page_skip_file()
     return 0;
 }
 
+static char * test_cursor_up()
+{
+    struct display_t *display;
+    struct search_t *search;
+    int terminal_line_nb;
+    char text[] = "this is the first line\nthis the second line\n";
+    const char *pattern = "line";
+
+    display = create_display();
+    search = create_search();
+    terminal_line_nb = 10;
+
+    parse_text(search, "fake_file", strlen(text), text, pattern);
+    display->cursor = 2;
+    move_cursor_up(display, search, terminal_line_nb);
+    mu_assert("test_cursor_up failed", display->cursor == 1);
+    free_search(search);
+    free_display(display);
+
+    return 0;
+}
+
 static char * all_tests() {
     mu_run_test(test_no_entry);
     mu_run_test(test_one_entry_on_the_first_line);
@@ -284,6 +306,7 @@ static char * all_tests() {
     mu_run_test(test_cursor_down_skip_file);
     mu_run_test(test_cursor_down_end_of_page);
     mu_run_test(test_cursor_down_end_of_page_skip_file);
+    mu_run_test(test_cursor_up);
     return 0;
 }
 
