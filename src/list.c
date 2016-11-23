f688fad0fd50fc4952f26f2ef002d50ce1f9c2d6
Jonathan Klee <klee.jonathan@gmail.com>
2016-11-23 23:44:03 +0100

    switch to GPL v3

--- src/list.c
+++ src/list.c
@@ -1,3 +1,21 @@
+/* Copyright (c) 2013-2016  Jonathan Klee
+
+This file is part of ngp.
+
+ngp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ngp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ngp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "list.h"
 
 struct list* create_list()
